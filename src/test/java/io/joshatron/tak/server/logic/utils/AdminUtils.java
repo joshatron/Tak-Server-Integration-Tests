622ceeff9e85f899691af0edb8ed75a34d1a919a
Joshua Leger <legermjoshua@gmail.com>
2019-03-08 17:46:41 -0800

    Create higher level util for admin endpoint

---
+++ src/test/java/io/joshatron/tak/server/logic/utils/AdminUtils.java
@@ -0,0 +1,75 @@
+package io.joshatron.tak.server.logic.utils;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.junit.Assert;
+
+import java.io.IOException;
+
+public class AdminUtils {
+
+    public static String initialize(HttpClient client, int expected) throws IOException {
+        HttpResponse response = HttpUtils.initializeAdminAccount(client);
+        Assert.assertEquals(expected, response.getStatusLine().getStatusCode());
+        //This is from the server default password
+        return "admin-password";
+    }
+
+    public static void changePassword(User user, String newPass, HttpClient client, int expected) throws IOException {
+        HttpResponse response;
+        if(user != null) {
+            response = HttpUtils.changeAdminPassword(user.getUsername(), user.getPassword(), newPass, client);
+        }
+        else {
+            response = HttpUtils.changeAdminPassword(null, null, newPass, client);
+        }
+        Assert.assertEquals(expected, response.getStatusLine().getStatusCode());
+        if(expected == HttpStatus.SC_NO_CONTENT) {
+            user.setPassword(newPass);
+        }
+    }
+
+    public static String resetUserPassword(User user, String toReset, HttpClient client, int expected) throws IOException {
+        HttpResponse response;
+        if(user != null) {
+            response = HttpUtils.resetUserPassword(user.getUsername(), user.getPassword(), toReset, client);
+        }
+        else {
+            response = HttpUtils.resetUserPassword(null, null, toReset, client);
+        }
+        Assert.assertEquals(expected, response.getStatusLine().getStatusCode());
+        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String contents = EntityUtils.toString(response.getEntity());
+            JSONObject json = new JSONObject(contents);
+
+            return json.getString("text");
+        }
+
+        return null;
+    }
+
+    public static void banUser(User user, String toBan, HttpClient client, int expected) throws IOException {
+        HttpResponse response;
+        if(user != null) {
+            response = HttpUtils.banUser(user.getUsername(), user.getPassword(), toBan, client);
+        }
+        else {
+            response = HttpUtils.banUser(null, null, toBan, client);
+        }
+        Assert.assertEquals(expected, response.getStatusLine().getStatusCode());
+    }
+
+    public static void unbanUser(User user, String toUnban, HttpClient client, int expected) throws IOException {
+        HttpResponse response;
+        if(user != null) {
+            response = HttpUtils.unbanUser(user.getUsername(), user.getPassword(), toUnban, client);
+        }
+        else {
+            response = HttpUtils.unbanUser(null, null, toUnban, client);
+        }
+        Assert.assertEquals(expected, response.getStatusLine().getStatusCode());
+    }
+}
